a62d3299adb21414f386f6db136001f0d4266f4f
Aman Mishra <xtreme.amx.45@gmail.com>
2026-05-31 07:50:43 +0530

    Remove duplicate navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/layout/Navbar";
+
 import Hero from "@/components/hero/Hero";
 import LatestChapters from "@/components/sections/LatestChapters";
 import FeaturedCharacters from "@/components/sections/FeaturedCharacters";
@@ -6,7 +6,6 @@ import FeaturedCharacters from "@/components/sections/FeaturedCharacters";
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <LatestChapters />
       <FeaturedCharacters />
